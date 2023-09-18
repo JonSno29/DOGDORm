2bd7ee98d999551f23f414ab48350631c0ee179c
JonSno29 <109987633+JonSno29@users.noreply.github.com>
2023-09-18 16:57:14 -0700

    added analytics

--- php/contact.php
+++ php/contact.php
@@ -1,71 +1,41 @@
 <?php
-header('Content-type: application/json');
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
 
-if($_POST)
-{
-    $to_email       = "snoverjon@gmail.com"; //Recipient email, Replace with own email here
-   
-    //check if its an ajax request, exit if not
-    if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
-       
-        $output = json_encode(array( //create JSON data
-            'type'=>'error',
-            'text' => 'Sorry Request must be Ajax POST'
-        ));
-        die($output); //exit script outputting json data
-    }
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'snoverjon@gmail.com';
 
-    //Sanitize input data using PHP filter_var().
-    $user_name      = filter_var($_POST["name"],    FILTER_SANITIZE_STRING);
-    $user_email     = filter_var($_POST["email"],   FILTER_SANITIZE_EMAIL);
-    $phone_number   = filter_var($_POST["phone"],   FILTER_SANITIZE_NUMBER_INT);
-    $message        = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+  if( file_exists($php_email_form = '../contact.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
 
-    //additional php validation
-    if(strlen($user_name)<4){ // If length is less than 4 it will output JSON error.
-        $output = json_encode(array('type'=>'error', 'text' => 'Name is too short or empty!'));
-        die($output);
-    }
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
 
-    if(!filter_var($user_email, FILTER_VALIDATE_EMAIL)){ //email validation
-        $output = json_encode(array('type'=>'error', 'text' => 'Please enter a valid email!'));
-        die($output);
-    }
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  */
 
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
 
-    if(!filter_var($phone_number, FILTER_SANITIZE_NUMBER_FLOAT)){ //check for valid numbers in phone number field
-        $output = json_encode(array('type'=>'error', 'text' => 'Enter only digits in phone number'));
-        die($output);
-    }
-
-    if(strlen($message)<3){ //check emtpy message
-        $output = json_encode(array('type'=>'error', 'text' => 'Too short message! Please enter something.'));
-        die($output);
-    }
-
-    //email subject
-    $subject ='New mail via contact form';
-
-    //email body
-    $message_body = $message."\r\n\r\n-".$user_name."\r\n\r\nEmail : ".$user_email."\r\nPhone Number : ". $phone_number ;
-   
-    //proceed with PHP email.
-    $headers = 'From: '.$user_name.'<'.$user_email.'>'."\r\n" .
-    'Reply-To: '.$user_name.'<'.$user_email.'>' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-   
-    $send_mail = mail($to_email, $subject, $message_body, $headers);
-   
-    if(!$send_mail)
-    {
-        //If mail couldn't be sent output error. Check your PHP email configuration (if it ever happens)
-        $output = json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please check your PHP mail configuration.'));
-        die($output);
-    }else{
-        $output = json_encode(array('type'=>'success', 'text' => 'Hi '.$user_name .', thank you for your email, we will get back to you shortly.'));
-        die($output);
-    }
-}
-
-
+  echo $contact->send();
 ?>
